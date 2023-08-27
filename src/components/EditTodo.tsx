95a6dbda86bb4c6031c678564408fcb26b57871d
unknown <alinamva>
2023-08-27 16:22:46 +0400

    deleting and editing todos

---
+++ src/components/EditTodo.tsx
@@ -0,0 +1,64 @@
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import { HomeProps } from "../pages/Home";
+import { useState } from "react";
+
+const EditTodo = ({
+  setEditForm,
+  setTodos,
+  selectedTodo,
+  todos,
+}: Pick<HomeProps, "setEditForm" | "setTodos" | "selectedTodo" | "todos">) => {
+  const handleEditForm = () => {
+    setEditForm(false);
+  };
+  const [todo, setTodo] = useState(todos[selectedTodo] || " ");
+  //   console.log(todo);
+  const handleEditSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const storedTodos = localStorage.getItem("todos");
+    const newTodos = storedTodos ? JSON.parse(storedTodos) : [];
+    if (selectedTodo >= 0 && selectedTodo < newTodos.length) {
+      newTodos[selectedTodo] = todo;
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      setTodos(newTodos);
+    }
+
+    setEditForm(false);
+  };
+
+  return (
+    <motion.form
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.2 }}
+      className="w-96 bg-slate-400 absolute p-10 rounded-2xl gap-3 flex flex-col"
+      onSubmit={handleEditSubmit}
+    >
+      <div className="cursor-pointer justify-end flex" onClick={handleEditForm}>
+        <XCircleIcon className="w-6" />
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="text-center">
+          <h2>Edit Todo</h2>
+        </div>
+        <div className="flex flex-col gap-3">
+          <label>
+            <input
+              type="text"
+              className="input input-bordered input-primary w-full max-w-xs"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+          </label>
+
+          <label>
+            <button className="btn btn-primary my-3">Edit</button>
+          </label>
+        </div>
+      </div>
+    </motion.form>
+  );
+};
+
+export default EditTodo;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import wave from "../assets/wave.png";
 import quotes from "../constants/Quotes";
 import { motion } from "framer-motion";
-import { TrashIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import {
+  ArrowRightOnRectangleIcon,
+  PencilSquareIcon,
+  TrashIcon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
 import Login from "../components/Login";
 import AddTodo from "../components/AddTodo";
+import EditTodo from "../components/EditTodo";
 
 export interface HomeProps {
   loginForm: boolean;
@@ -15,6 +22,10 @@ export interface HomeProps {
   setTodoForm: (todoform: boolean) => void;
   todos: string[];
   setTodos: React.Dispatch<React.SetStateAction<string[]>>;
+  editForm: boolean;
+  setEditForm: (editForm: boolean) => void;
+  selectedTodo: number;
+  setSelectedTodo: (selectedTodo: number) => void;
 }
 
 const Home = () => {
@@ -22,9 +33,13 @@ const Home = () => {
   const [userName, setUserName] = useState("");
   const [todoForm, setTodoForm] = useState(false);
   const [todos, setTodos] = useState<string[]>([]);
+  const [dropDown, setDropDown] = useState(false);
+  const [editForm, setEditForm] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState(-1);
 
   const handleLoginForm = () => {
     setLoginForm(true);
+    setDropDown(false);
   };
   const [randomQuote, setRandomQuote] = useState(
     quotes[Math.floor(Math.random() * quotes.length)]
@@ -50,9 +65,26 @@ const Home = () => {
   const handleLogOut = () => {
     localStorage.removeItem("userName");
     setUserName("");
+    localStorage.removeItem("todos");
+    setTodos([]);
   };
   const handleTodoForm = () => {
     setTodoForm(true);
+  };
+
+  const handleDeleteTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  };
+
+  const handleDropDown = () => {
+    setDropDown((showDropDown) => !showDropDown);
+  };
+  const handleEditTodo = (index: number) => {
+    setSelectedTodo(index);
+    setEditForm(true);
   };
 
   return (
@@ -78,10 +110,26 @@ const Home = () => {
             {randomQuote}
           </motion.p>{" "}
         </div>
-        {userName && <h2 onClick={handleLogOut}>Logout</h2>}
-        <div onClick={handleLoginForm}>
+        <div onClick={handleDropDown}>
           <UserCircleIcon className="w-24 h-24 cursor-pointer hover:text-white duration-150 text-gray-400" />
         </div>
+        {dropDown && (
+          <div className="w-40 p-6 bg-slate-100 absolute text-indigo-950 rounded-2xl left-[85.3%] top-36">
+            <ul className="gap-3 flex flex-col">
+              <li onClick={handleLoginForm}>
+                <UserIcon className="w-6" />
+                Profile
+              </li>
+
+              {userName && (
+                <li className="text-red-500" onClick={handleLogOut}>
+                  <ArrowRightOnRectangleIcon className="w-6" />
+                  Logout
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
       </header>
       {loginForm && (
         <div className="fixed flex inset-0 bg-black/40  items-center justify-center">
@@ -93,9 +141,26 @@ const Home = () => {
           <AddTodo setTodoForm={setTodoForm} setTodos={setTodos} />
         </div>
       )}
-      {todos.length == 0 && <div>You dont have any todo</div>}
-
-      {todos && (
+      {editForm && (
+        <div className="fixed flex inset-0 bg-black/40  items-center justify-center">
+          <EditTodo
+            setEditForm={setEditForm}
+            setTodos={setTodos}
+            selectedTodo={selectedTodo}
+            todos={todos}
+          />
+        </div>
+      )}
+      <div className="m-auto w-96  gap-5 flex flex-col text-center ">
+        {todos.length == 0 && userName ? (
+          <h2>You dont have any todos!</h2>
+        ) : todos.length !== 0 && userName ? (
+          <div></div>
+        ) : (
+          <h2>Enter to your profile first!</h2>
+        )}
+      </div>
+      {todos && todos.length !== 0 && (
         <div className=" m-auto w-96  gap-5 flex flex-col ">
           <div className="text-center">
             <h2>My Todo List</h2>
@@ -104,7 +169,14 @@ const Home = () => {
             {todos.map((todo, index) => (
               <li key={index} className="flex w-full justify-between">
                 <span>{todo}</span>
-                <TrashIcon className="w-6 cursor-pointer" />
+                <div className="flex gap-2">
+                  <div onClick={() => handleDeleteTodo(index)}>
+                    <TrashIcon className="w-6 cursor-pointer" />
+                  </div>
+                  <div onClick={() => handleEditTodo(index)}>
+                    <PencilSquareIcon className="w-6" />
+                  </div>
+                </div>
               </li>
             ))}
           </ul>
